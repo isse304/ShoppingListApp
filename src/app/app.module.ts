0974dcea92b80eb591a15a0ff086a0b447e7939f
isse304 <101603508+isse304@users.noreply.github.com>
2024-05-01 07:50:24 -0500

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingEditComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
